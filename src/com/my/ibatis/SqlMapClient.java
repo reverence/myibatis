1cee2c68d1039b579b980686eb4dc774021a8624
reverence <reverence_cz@163.com>
2018-01-05 19:40:45 +0800

    Update SqlMapClient.java

--- src/com/my/ibatis/SqlMapClient.java
+++ src/com/my/ibatis/SqlMapClient.java
@@ -1,41 +1,15 @@
 package com.my.ibatis;
 
-import java.sql.SQLException;
-import java.util.List;
 
 /**
  * Created by tufei on 2017/12/29.
  */
-public interface SqlMapClient {//暴露给客户端
-    /**
-     * insert
-     * @param sql
-     * @param object
-     * @return
-     */
-    public Object insert(String sql,Object object);
-
-    /**
-     * select one
-     * @param sql
-     * @param parameterObject
-     * @return
-     */
-    public Object selectForObject(String sql,Object parameterObject) throws SQLException;
-
-    /**
-     * select list
-     * @param sql
-     * @param parameterObject
-     * @return
-     */
-    public List<Object> selectForList(String sql, Object parameterObject);
-
-    /**
-     * update
-     * @param sql
-     * @param parameterObject
-     * @return
-     */
-    public int update(String sql,Object parameterObject);
+public interface SqlMapClient extends SqlMapExecutor{//暴露给客户端
+	
+	/**
+	 * 开启一个会话
+	 * @return
+	 */
+	public SqlMapSession open();
+    
 }
