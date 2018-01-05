079d4d777e2cc2420cdec28b1b95320dfb3a466e
reverence <reverence_cz@163.com>
2018-01-05 19:46:49 +0800

    Update TypeHandlerFactory.java

--- src/com/my/ibatis/TypeHandlerFactory.java
+++ src/com/my/ibatis/TypeHandlerFactory.java
@@ -2,7 +2,6 @@ package com.my.ibatis;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by tufei on 2018/1/1.
@@ -12,8 +11,10 @@ public class TypeHandlerFactory {
     private static final Map<Object ,TypeHandler> handlerMap = new HashMap<Object,TypeHandler>();
 
     static {
-        handlerMap.put(Integer.class,new IntegerTypeHandler());//todo 处理
+        handlerMap.put(Integer.class,new IntegerTypeHandler());//todo 可以考虑放入配置文件
         handlerMap.put("int",new IntegerTypeHandler());
+        handlerMap.put(String.class, new StringTypeHandler());
+        handlerMap.put(int.class, new IntegerTypeHandler());
     }
 
     public static TypeHandler getTypeHandler(Object obj){
