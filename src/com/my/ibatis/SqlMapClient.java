a7f5d582ec5685ed4d7d3eb03bcc85eb4b91bc67
reverence <reverence_cz@163.com>
2018-01-07 20:15:01 +0800

    Update SqlMapClient.java

--- src/com/my/ibatis/SqlMapClient.java
+++ src/com/my/ibatis/SqlMapClient.java
@@ -4,7 +4,7 @@ package com.my.ibatis;
 /**
  * Created by tufei on 2017/12/29.
  */
-public interface SqlMapClient extends SqlMapExecutor{//暴露给客户端
+public interface SqlMapClient extends SqlMapExecutor,SqlMapTransactionManager{//暴露给客户端
 	
 	/**
 	 * 开启一个会话
